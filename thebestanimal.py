002cb55fab4bfb0468f4dd04369129cd811b8610
NightKoneko <62818210+NightKoneko@users.noreply.github.com>
2021-04-14 15:47:47 -0400

    Add files via upload

---
+++ thebestanimal.py
@@ -0,0 +1,24 @@
+while True:
+    from random import random
+    cat = value = random()
+    dog = value = random()
+    threshold = 0.25
+    monkey = value = random()
+
+    if abs(cat - dog) > monkey:
+        print("actually monkeys are the best")
+    
+    if not abs(cat - dog) > monkey:
+        if abs(cat - dog) >= threshold and cat > dog:
+            print("cat is better than dog")
+    
+        if abs(cat - dog) >= threshold and cat < dog:
+            print("dog is better than cat")
+    
+        if abs(cat - dog) < threshold:
+            print("they are equally good")
+    cont = input("Another one? yes/no > ")
+    while cont.lower() not in ("yes","no"):
+        cont = input("Another one? yes/no > ")
+    if cont == "no":
+        break
